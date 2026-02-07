7bf0941f687a6f767b219a7557d9e75a0f165c36
SOFTMAXTER <softmaxter@hotmail.com>
2026-02-06 20:58:10 -0600

    Update window title to v1.4.6

--- Run.bat
+++ Run.bat
@@ -30,7 +30,7 @@ echo Privilegios de Administrador obtenidos. Ejecutando AdminImagenOffline..
 echo.
 
 :: Establecer el titulo de la ventana
-title AdminImagenOffline v1.4.5 by SOFTMAXTER
+title AdminImagenOffline v1.4.6 by SOFTMAXTER
 
 mode con: cols=110 lines=35
 
